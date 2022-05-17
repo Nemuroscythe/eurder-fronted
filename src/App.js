d3488e4baa26c0b7539108675b508f83b12b1e42
Jeremy Alen <alen_jeremy@hotmail.com>
2022-05-17 15:32:18 +0200

    Adding icon the proper way CU3 - CreateCustomer.jsx empty component CU3 - Header.jsx empty component CU3 - Footer.jsx empty component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import CreateCustomer from "./component/CreateCustomer";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
 
 function App() {
     return (<>
         <div className="App">
-
+            <Header/>
+            <CreateCustomer/>
+            <Footer/>
         </div>
     </>);
 }
---
+++ src/component/CreateCustomer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function CreateCustomer() {
+    return (<>
+        <p>Create customer component</p>
+    </>)
+}

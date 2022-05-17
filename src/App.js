73eccf22666cad75a5b104da90db36adb381de77
Jeremy Alen <alen_jeremy@hotmail.com>
2022-05-17 16:33:28 +0200

    Adding title and logo to the Header.jsx CU3 - Adding routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import CreateCustomer from "./component/CreateCustomer";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (<>
         <div className="App">
             <Header/>
-            <CreateCustomer/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/"} component={CreateCustomer}/>
+                    <Route path={"/create-customer"} component={CreateCustomer}/>
+                    <Route path={"*"} component={CreateCustomer}/>
+                </Routes>
+            </BrowserRouter>
             <Footer/>
         </div>
     </>);
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Header() {
     return (<>
-        <p>Header</p>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="http://localhost:3000">
+                    <img src={require("../assets/images/favicon.ico")} alt={'logo'}/>
+                    ÖRDER
+                </a>
+
+                <div className="container-fluid">
+                    {/*<button type="button"><Link to="/create-customer">Navigation</Link></button>*/}
+                </div>
+            </div>
+        </nav>
     </>)
 }
